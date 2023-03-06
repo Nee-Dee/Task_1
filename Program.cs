8e8f71d0fae3024f0fc8d666979856bb83febdbd
Tom <Tom@NEEDEEPC>
2023-03-06 20:11:04 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿static string reverse(string str)
+{
+    char[] input_array = str.ToCharArray();
+    Array.Reverse(input_array);
+    return new string(input_array);
+}
+
+
+string input_string;
+string substring1;
+input_string = Console.ReadLine();
+if (input_string.Length % 2 == 0)
+{
+    string substring2;
+    substring1 = reverse(input_string.Substring(0, input_string.Length / 2));
+    substring2 = reverse(input_string.Substring(input_string.Length / 2));
+    Console.WriteLine(substring1 + substring2);
+}
+else
+{
+    substring1 = reverse(input_string);
+    Console.WriteLine(substring1 + input_string);
+}
